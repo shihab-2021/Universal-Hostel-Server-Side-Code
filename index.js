709fdf8f73a537f579ff5f23b40ac97a2bab94e4
Christos Uster <christos.uster@gmail.com>
2023-03-08 19:08:03 +0600

    payment update api

--- index.js
+++ index.js
@@ -248,6 +248,33 @@ async function run() {
       res.json(payments);
     });
 
+    // for getting all payments
+    app.put("/payments", async (req, res) => {
+      const paymentID = req.body.id;
+      const amount = req.body.amount;
+      const paymentType = req.body.amount;
+      const userPaymentRecord = await paymentCollection.findOne({
+        _id: new ObjectId(paymentID),
+      });
+      const paymentQuery = { _id: new ObjectId(paymentID) };
+      const paymentDoc = {
+        $push: {
+          paymentHistory: {
+            date: today,
+            amount: parseInt(amount),
+          },
+        },
+        $set: {
+          rent: 0,
+          due: 0,
+        },
+      };
+      const paymentResult = await paymentCollection.updateOne(
+        paymentQuery,
+        paymentDoc
+      );
+    });
+
     // payment post api
     app.post("/notice", async (req, res) => {
       const cursor = await noticeCollection.insertOne(req.body);
@@ -261,109 +288,106 @@ async function run() {
       res.json(notices);
     });
 
-    // for getting payment by id
-    app.get("/payments/:id", async (req, res) => {
-      const query = { uid: req?.params?.id };
-      const cursor = await paymentCollection.findOne(query);
-      res.json(cursor);
-    });
+    // // for getting payment by id
+    // app.get("/payments/:id", async (req, res) => {
+    //   const query = { uid: req?.params?.id };
+    //   const cursor = await paymentCollection.findOne(query);
+    //   res.json(cursor);
+    // });
 
-    app.get("/ssl-request", async (req, res) => {
-      /**
-       * Create ssl session request
-       */
+    // app.get("/ssl-request", async (req, res) => {
+    //   /**
+    //    * Create ssl session request
+    //    */
 
-      const data = {
-        total_amount: 100,
-        currency: "BDT",
-        tran_id: "REF123",
-        success_url: `${process.env.ROOT}/ssl-payment-success`,
-        fail_url: `${process.env.ROOT}/ssl-payment-fail`,
-        cancel_url: `${process.env.ROOT}/ssl-payment-cancel`,
-        shipping_method: "No",
-        product_name: "Computer.",
-        product_category: "Electronic",
-        product_profile: "general",
-        cus_name: "Customer Name",
-        cus_email: "cust@yahoo.com",
-        cus_add1: "Dhaka",
-        cus_add2: "Dhaka",
-        cus_city: "Dhaka",
-        cus_state: "Dhaka",
-        cus_postcode: "1000",
-        cus_country: "Bangladesh",
-        cus_phone: "01711111111",
-        cus_fax: "01711111111",
-        multi_card_name: "mastercard",
-        value_a: "ref001_A",
-        value_b: "ref002_B",
-        value_c: "ref003_C",
-        value_d: "ref004_D",
-        ipn_url: `${process.env.ROOT}/ssl-payment-notification`,
-      };
+    //   const data = {
+    //     total_amount: 100,
+    //     currency: "BDT",
+    //     tran_id: "REF123",
+    //     success_url: `${process.env.ROOT}/ssl-payment-success`,
+    //     fail_url: `${process.env.ROOT}/ssl-payment-fail`,
+    //     cancel_url: `${process.env.ROOT}/ssl-payment-cancel`,
+    //     shipping_method: "No",
+    //     product_name: "Computer.",
+    //     product_category: "Electronic",
+    //     product_profile: "general",
+    //     cus_name: "Customer Name",
+    //     cus_email: "cust@yahoo.com",
+    //     cus_add1: "Dhaka",
+    //     cus_add2: "Dhaka",
+    //     cus_city: "Dhaka",
+    //     cus_state: "Dhaka",
+    //     cus_postcode: "1000",
+    //     cus_country: "Bangladesh",
+    //     cus_phone: "01711111111",
+    //     cus_fax: "01711111111",
+    //     multi_card_name: "mastercard",
+    //     value_a: "ref001_A",
+    //     value_b: "ref002_B",
+    //     value_c: "ref003_C",
+    //     value_d: "ref004_D",
+    //     ipn_url: `${process.env.ROOT}/ssl-payment-notification`,
+    //   };
 
-      const sslcommerz = new SSLCommerzPayment(
-        process.env.STORE_ID,
-        process.env.STORE_PASSWORD,
-        false
-      ); //true for live default false for sandbox
-      sslcommerz.init(data).then((data) => {
-        //process the response that got from sslcommerz
-        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+    //   const sslcommerz = new SSLCommerzPayment(
+    //     process.env.STORE_ID,
+    //     process.env.STORE_PASSWORD,
+    //     false
+    //   ); //true for live default false for sandbox
+    //   sslcommerz.init(data).then((data) => {
+    //     //process the response that got from sslcommerz
+    //     //https://developer.sslcommerz.com/doc/v4/#returned-parameters
 
-        if (data?.GatewayPageURL) {
-          return res.status(200).redirect(data?.GatewayPageURL);
-        } else {
-          return res.status(400).json({
-            message: "Session was not successful",
-          });
-        }
-      });
-    });
+    //     if (data?.GatewayPageURL) {
+    //       return res.status(200).redirect(data?.GatewayPageURL);
+    //     } else {
+    //       return res.status(400).json({
+    //         message: "Session was not successful",
+    //       });
+    //     }
+    //   });
+    // });
 
-    app.post("/ssl-payment-notification", async (req, res) => {
-      /**
-       * If payment notification
-       */
+    // app.post("/ssl-payment-notification", async (req, res) => {
+    //   /**
+    //    * If payment notification
+    //    */
 
-      return res.status(200).json({
-        data: req.body,
-        message: "Payment notification",
-      });
-    });
+    //   return res.status(200).json({
+    //     data: req.body,
+    //     message: "Payment notification",
+    //   });
+    // });
 
-    app.post("/ssl-payment-success", async (req, res) => {
-      /**
-       * If payment successful
-       */
+    // app.post("/ssl-payment-success", async (req, res) => {
 
-      return res.status(200).json({
-        data: req.body,
-        message: "Payment success",
-      });
-    });
+    //   return res.status(200).json({
+    //     data: req.body,
+    //     message: "Payment success",
+    //   });
+    // });
 
-    app.post("/ssl-payment-fail", async (req, res) => {
-      /**
-       * If payment failed
-       */
+    // app.post("/ssl-payment-fail", async (req, res) => {
+    //   /**
+    //    * If payment failed
+    //    */
 
-      return res.status(200).json({
-        data: req.body,
-        message: "Payment failed",
-      });
-    });
+    //   return res.status(200).json({
+    //     data: req.body,
+    //     message: "Payment failed",
+    //   });
+    // });
 
-    app.post("/ssl-payment-cancel", async (req, res) => {
-      /**
-       * If payment cancelled
-       */
+    // app.post("/ssl-payment-cancel", async (req, res) => {
+    //   /**
+    //    * If payment cancelled
+    //    */
 
-      return res.status(200).json({
-        data: req.body,
-        message: "Payment cancelled",
-      });
-    });
+    //   return res.status(200).json({
+    //     data: req.body,
+    //     message: "Payment cancelled",
+    //   });
+    // });
 
     //-------------------------------------------------------------
     //-------------------------------------------------------------
